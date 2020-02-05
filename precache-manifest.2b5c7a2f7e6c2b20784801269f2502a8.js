66c85ad1b33f666cc480b3a0f8058a97754bef09
rkukin <romakukin@gmail.com>
2020-02-05 08:29:26 +0200

    Updates

---
+++ precache-manifest.2b5c7a2f7e6c2b20784801269f2502a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5720abc260a1fada0a0b36ca0334e7d5",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "9feedf2515098369b37f",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "9feedf2515098369b37f",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.671f2b92.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.671f2b92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4185cc6963db4a93417",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.e72af750.chunk.js"
+  },
+  {
+    "revision": "9ed00262ab067fcfa080",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.5a6095d3.js"
+  }
+]);
